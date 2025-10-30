2d2554193ca3d5e5d480eca156d7aafade893471
TomKrawczyk <138514334+TomKrawczyk@users.noreply.github.com>
2025-10-30 21:18:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/prezentacja_doradca/', // Zmień na nazwę swojego repo
+  base: 'https://github.com/TomKrawczyk/prezentacja_doradca', // Zmień na nazwę swojego repo
   server: {
     host: "::",
     port: 8080,
